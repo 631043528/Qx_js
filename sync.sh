35a8d6374880450fee81a576b314f12f9a951db3
631043528 <631043528@qq.com>
2026-01-22 05:47:10 +0800

    Update at 2026-01-22 05:47

--- sync.sh
+++ sync.sh
@@ -3,22 +3,24 @@
 
 TARGET="/vol1/1000/git/Qx_js"
 
+
 cd "$TARGET" || exit
 
-echo "--- Ready: $TARGET ---"
+echo "--- 准备同步目录: $TARGET ---"
 
 
 git add .
 
 
 if git diff-index --quiet HEAD --; then
-  
+    echo "提示：没有检测到任何变动，跳过推送。"
 else
- 
+    
     msg="Update at $(date +'%Y-%m-%d %H:%M')"
     git commit -m "$msg"
     
-    git push
     
-    echo "--- Success！ ---"
+    echo "正在推送到 GitHub..."
+    git push
+    echo "--- 同步完成！ ---"
 fi
